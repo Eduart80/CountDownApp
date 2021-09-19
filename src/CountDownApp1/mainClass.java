34433ee3bcd0f64471d83ad3c2e99561c3060b29
edd <zeedymarina@gmail.com>
2021-09-19 01:07:43 -0400

    three

--- src/CountDownApp1/mainClass.java
+++ src/CountDownApp1/mainClass.java
@@ -1,7 +1,33 @@
 package CountDownApp1;
 
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class mainClass {
+
+    static int internal;
+    static Timer timer;
+
     public static void main(String[] args) {
-        System.out.println("Hello boss");
+
+        System.out.print("Enter seconds: ");
+        Scanner scanner = new Scanner(System.in);
+        String res = scanner.nextLine();
+        int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
+        internal = Integer.parseInt(res);
+        System.out.println(res);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                System.out.println(setInterval());
+            }
+        }, delay, period);
+    }
+    private static final int setInterval() {
+        if (internal == 1)
+            timer.cancel();
+        return --internal;
     }
 }
